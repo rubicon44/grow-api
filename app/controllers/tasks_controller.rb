163b5e927a129a916f8299582fe8890a0b0471ad
rubicon44 <rub.b.c44@gmail.com>
2020-11-26 17:13:06 +0900

    create .editorconfig && add action to tasks controller && add tasks routing to routes.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,35 @@
 class TasksController < ApplicationController
+  def index
+    @task = Task.all
+    render json: @task
+  end
+
+  def show
+    render json: @task
+  end
+
+  def create
+    @task = Task.create(task: params[:task])
+
+    if @man.save
+      render json: @task, status: 201
+    else
+      render json: @task.errors, status: 500
+    end
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(task: params[:task])
+    render json: @task
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      head :no_content, status: :ok
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
+  end
 end
