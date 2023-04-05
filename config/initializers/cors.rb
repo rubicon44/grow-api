206b19e24c25e2429d4a0e16046752f4535e8e8e
rubicon44 <rub.b.c44@gmail.com>
2023-04-06 03:35:10 +0900

    fix

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -13,6 +13,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     #   headers: :any,
     #   methods: [:get, :post, :put, :patch, :delete, :options, :head]
     origins 'https://grow-gilt.vercel.app'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], expose: ['Access-Control-Allow-Origin'], credentials: true
   end
 end
