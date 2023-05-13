69b3f43e8dae55dc984a504aac5a66380972db3f
rubicon44 <rub.b.c44@gmail.com>
2023-05-13 15:40:38 +0900

    Create users_controllers_spec.

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,7 +1,6 @@
 module V1
   class UsersController < ApiController
     skip_before_action :check_authenticate!, only: %i(create), raise: false
-    before_action :require_user_owner, only: [:update]
 
     # def index
     #   users = User.all
@@ -11,31 +10,65 @@ module V1
 
     def show
       user = User.find_by(username: params[:username])
-      user_data = UserSerializer.new(user).as_json
-      user_data[:tasks] = ActiveModel::Serializer::CollectionSerializer.new(user.tasks.order('tasks.id DESC'), each_serializer: TaskSerializer)
-      user_data[:liked_tasks] = ActiveModel::Serializer::CollectionSerializer.new(user.like_tasks.order('likes.id DESC'), each_serializer: TaskSerializer)
-      render json: user_data, status: 200
+
+      # todo: エラー出力の方法をapi_controllerに任せるか検討
+      if user.nil?
+        render json: { errors: 'User not found' }, status: 404
+      else
+        user_data = UserSerializer.new(user).as_json
+        user_data[:tasks] = ActiveModel::Serializer::CollectionSerializer.new(user.tasks.order('tasks.id DESC'), each_serializer: TaskSerializer)
+        user_data[:liked_tasks] = ActiveModel::Serializer::CollectionSerializer.new(user.like_tasks.order('likes.id DESC'), each_serializer: TaskSerializer)
+        render json: user_data, status: 200
+      end
     end
 
     def followings
       user = User.find_by(username: params[:username])
-      followings = user.followings.order('relationships.id DESC')
-      render json: { followings: ActiveModel::Serializer::CollectionSerializer.new(followings, each_serializer: UserSerializer) }, status: 200
+
+      if user.nil?
+        render json: { error: 'User not found' }, status: 404
+      else
+        followings = user.followings.order('relationships.id DESC')
+        render json: { followings: ActiveModel::Serializer::CollectionSerializer.new(followings, each_serializer: UserSerializer) }, status: 200
+      end
     end
 
     def followers
       user = User.find_by(username: params[:username])
-      followers = user.followers.order('relationships.id DESC')
-      render json: { followers: ActiveModel::Serializer::CollectionSerializer.new(followers, each_serializer: UserSerializer) }, status: 200
+
+      if user.nil?
+        render json: { error: 'User not found' }, status: 404
+      else
+        followers = user.followers.order('relationships.id DESC')
+        render json: { followers: ActiveModel::Serializer::CollectionSerializer.new(followers, each_serializer: UserSerializer) }, status: 200
+      end
     end
 
     def create
       user = User.new(params_user_create)
+
+      if user.nickname.nil? || user.username.nil? || user.email.nil? || user.firebase_id.nil?
+        render json: { errors: user.errors.full_messages }, status: 422
+        return
+      end
+
       render json: {}, status: 204 if user.save
     end
 
     def update
       user = User.find_by(username: params[:username])
+
+      if user.nil?
+        render json: { errors: 'User not found' }, status: 404
+        return
+      end
+
+      current_user_id = params[:current_user_id].to_i
+      unless require_user_authorization(user.id, current_user_id)
+        render json: { errors: "You are not authorized to #{request.method.downcase} this user" }, status: 403
+        return
+      end
+
       render json: {}, status: 204 if user.update(params_user_update)
     end
 
@@ -52,11 +85,11 @@ module V1
       params.require(:user).permit(:nickname, :username, :bio)
     end
 
-    def require_user_owner
-      user = User.find_by(username: params[:username])
-      current_user_id = params[:current_user_id]
-      unless user.id == current_user_id
-        render json: { errors: "You are not authorized to update this user" }, status: :forbidden
+    def require_user_authorization(user_id, current_user_id)
+      if request.method == 'PUT'
+        return user_id == current_user_id
+      elsif request.method == 'DELETE'
+        return user_id == current_user_id
       end
     end
   end
--- spec/requests/v1/users_controllers_spec.rb
+++ spec/requests/v1/users_controllers_spec.rb
@@ -1,10 +1,364 @@
 require 'rails_helper'
 
-RSpec.describe "V1::UsersControllers", type: :request do
-  describe "GET /v1/users_controllers" do
-    it "works! (now write some real specs)" do
-      get v1_users_controllers_path
-      expect(response).to have_http_status(200)
+# todo: 認証エラーの際は、401を返す(following時になっていない。)
+# todo: contextとitのテストケース文言を揃える
+
+RSpec.describe V1::UsersController, type: :request do
+  # todo: ユーザーを最初にまとめるか、それぞれのテストケースで作成するか検討する。
+  let!(:user1) { FactoryBot.create(:user, nickname: "user1", username: "user1", bio: 'user1') }
+  let!(:user2) { FactoryBot.create(:user, nickname: "user2", username: "user2", bio: 'user2') }
+  let!(:headers1) { { 'Authorization' => JsonWebToken.encode(user_email: user1.email) } }
+  let!(:headers2) { { 'Authorization' => JsonWebToken.encode(user_email: user2.email) } }
+  let!(:task1_by_user1) { FactoryBot.create(:task, title: 'task1_by_user1', user: user1) }
+  let!(:task2_by_user1) { FactoryBot.create(:task, title: 'task2_by_user1', user: user1) }
+  let!(:like_by_user1) { FactoryBot.create(:like, user_id: user1.id, task_id: task1_by_user1.id) }
+
+  describe 'GET #show (not logged in)' do
+    it 'returns 401' do
+      get "/v1/#{user1.username}"
+      expect(response).to have_http_status(401)
+      expect(response.body).to eq("{\"errors\":\"Authorization token is missing\"}")
+    end
+
+    it 'returns 401 error if the user does not exist' do
+      get "/v1/nonexistent_user"
+      expect(response).to have_http_status(401)
+      expect(response.body).to eq("{\"errors\":\"Authorization token is missing\"}")
     end
   end
+
+  describe 'GET #show (logged in)' do
+    context 'when user1 is the logged-in user' do
+      it 'returns the user details' do
+        get "/v1/#{user1.username}", headers: headers1
+        expect(response).to have_http_status(200)
+
+        user_data = JSON.parse(response.body)
+        expect(user_data['nickname']).to eq('user1')
+        expect(user_data['username']).to eq('user1')
+        expect(user_data['bio']).to eq('user1')
+
+        tasks = user_data['tasks']
+        expect(tasks.length).to eq(2)
+        expect(tasks[0]['title']).to eq('task2_by_user1')
+        expect(tasks[1]['title']).to eq('task1_by_user1')
+
+        liked_tasks = user_data['liked_tasks']
+        expect(liked_tasks.length).to eq(1)
+        expect(liked_tasks[0]['title']).to eq('task1_by_user1')
+      end
+    end
+
+    context 'when user2 is the logged-in user' do
+      it 'returns the user details' do
+        get "/v1/#{user2.username}", headers: headers2
+        expect(response).to have_http_status(200)
+
+        user_data = JSON.parse(response.body)
+        expect(user_data['nickname']).to eq('user2')
+        expect(user_data['username']).to eq('user2')
+        expect(user_data['bio']).to eq('user2')
+
+        tasks = user_data['tasks']
+        expect(tasks).to eq([])
+
+        liked_tasks = user_data['liked_tasks']
+        expect(liked_tasks).to eq([])
+      end
+    end
+
+    context 'when the requested user does not exist' do
+      it 'returns 404 error' do
+        get "/v1/nonexistent_user", headers: headers1
+        expect(response).to have_http_status(404)
+        expect(response.body).to eq("{\"errors\":\"User not found\"}")
+      end
+    end
+  end
+
+  describe 'GET #followings (not logged in)' do
+    let!(:follow_relationship) { FactoryBot.create(:relationship, following: user1, follower: user2) }
+    it 'returns 401' do
+      get "/v1/#{user1.username}/followings"
+      expect(response).to have_http_status(401)
+      expect(response.body).to eq("{\"errors\":\"Authorization token is missing\"}")
+    end
+
+    it 'returns 401 error if the user does not exist' do
+      get "/v1/nonexistent_user/followings"
+      expect(response).to have_http_status(401)
+      expect(response.body).to eq("{\"errors\":\"Authorization token is missing\"}")
+    end
+  end
+
+  describe 'GET #followings (logged in)' do
+    context 'when user has followings' do
+      let!(:follow_relationship) { FactoryBot.create(:relationship, following: user1, follower: user2) }
+      context 'when user1 is the logged-in user' do
+        it 'returns the user\'s followings' do
+          get "/v1/#{user1.username}/followings", headers: headers1
+          expect(response).to have_http_status(200)
+
+          followings_data = JSON.parse(response.body)['followings']
+          expect(followings_data.length).to eq(1)
+          expect(followings_data[0]['username']).to eq('user2')
+        end
+      end
+
+      context 'when user2 is the logged-in user' do
+        it 'returns the user\'s followings' do
+          get "/v1/#{user2.username}/followings", headers: headers2
+          expect(response).to have_http_status(200)
+
+          followings_data = JSON.parse(response.body)['followings']
+          expect(followings_data).to eq([])
+        end
+      end
+    end
+
+    context 'when user has no followings' do
+      let!(:user) { FactoryBot.create(:user, username: "user") }
+      let!(:headers) { { 'Authorization' => JsonWebToken.encode(user_email: user.email) } }
+      it 'returns 200 with an empty array' do
+        get "/v1/#{user.username}/followings", headers: headers
+        expect(response).to have_http_status(200)
+
+        followings_data = JSON.parse(response.body)['followings']
+        expect(followings_data).to be_empty
+      end
+    end
+
+    context 'when attempting to get followings of a non-existing user' do
+      it 'returns 404 error' do
+        get "/v1/non_existing_user/followings", headers: headers1
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+  describe 'GET #followers (not logged in)' do
+    let!(:follow_relationship) { FactoryBot.create(:relationship, following: user1, follower: user2) }
+    it 'returns 401' do
+      get "/v1/#{user1.username}/followers"
+      expect(response).to have_http_status(401)
+      expect(response.body).to eq("{\"errors\":\"Authorization token is missing\"}")
+    end
+
+    it 'returns 401 error if the user does not exist' do
+      get "/v1/nonexistent_user/followers"
+      expect(response).to have_http_status(401)
+      expect(response.body).to eq("{\"errors\":\"Authorization token is missing\"}")
+    end
+  end
+
+  describe 'GET #followers (logged in)' do
+    context 'when user has followers' do
+      let!(:follow_relationship) { FactoryBot.create(:relationship, following: user1, follower: user2) }
+      context 'when user1 is the logged-in user' do
+        it 'returns the user\'s followers' do
+          get "/v1/#{user2.username}/followers", headers: headers2
+          expect(response).to have_http_status(200)
+
+          followers_data = JSON.parse(response.body)['followers']
+          expect(followers_data.length).to eq(1)
+          expect(followers_data[0]['username']).to eq('user1')
+        end
+      end
+
+      context 'when user2 is the logged-in user' do
+        it 'returns the user\'s followers' do
+          get "/v1/#{user1.username}/followers", headers: headers1
+          expect(response).to have_http_status(200)
+
+          followers_data = JSON.parse(response.body)['followers']
+          expect(followers_data).to eq([])
+        end
+      end
+    end
+
+    context 'when user has no followers' do
+      let!(:user) { FactoryBot.create(:user, username: "user") }
+      let!(:headers) { { 'Authorization' => JsonWebToken.encode(user_email: user.email) } }
+      it 'returns 200 with an empty array' do
+        get "/v1/#{user.username}/followers", headers: headers
+        expect(response).to have_http_status(200)
+
+        followers_data = JSON.parse(response.body)['followers']
+        expect(followers_data).to be_empty
+      end
+    end
+
+    context 'when attempting to get followers of a non-existing user' do
+      it 'returns 404 error' do
+        get "/v1/non_existing_user/followers", headers: headers1
+        expect(response).to have_http_status(404)
+      end
+    end
+  end
+
+  describe 'POST #create' do
+    it 'creates a new user' do
+      user_params = {
+        user: {
+          nickname: 'John',
+          username: 'john123',
+          email: 'john@example.com',
+          firebase_id: 'firebase123'
+        }
+      }
+
+      # user作成時のみheader認証は省略。
+      post '/v1/users', params: user_params
+      expect(response).to have_http_status(204)
+
+      user = User.last
+      expect(user.nickname).to eq('John')
+      expect(user.username).to eq('john123')
+      expect(user.email).to eq('john@example.com')
+      expect(user.firebase_id).to eq('firebase123')
+    end
+
+    context 'with invalid params' do
+      it 'if nickname is missing' do
+        user_params = {
+          user: {
+            nickname: nil,
+            username: 'john123',
+            email: 'john@example.com',
+            firebase_id: 'firebase123'
+          }
+        }
+
+        post '/v1/users', params: user_params
+        expect(response).to have_http_status(422)
+      end
+
+      it 'if username is missing' do
+        user_params = {
+          user: {
+            username: nil,
+            nickname: 'John',
+            email: 'john@example.com',
+            firebase_id: 'firebase123'
+          }
+        }
+
+        post '/v1/users', params: user_params
+        expect(response).to have_http_status(422)
+      end
+
+      it 'if email is missing' do
+        user_params = {
+          user: {
+            nickname: 'John',
+            username: 'john123',
+            email: nil,
+            firebase_id: 'firebase123'
+          }
+        }
+
+        post '/v1/users', params: user_params
+        expect(response).to have_http_status(422)
+      end
+
+      it 'if firebase_id is missing' do
+        user_params = {
+          user: {
+            nickname: 'John',
+            username: 'john123',
+            email: 'john@example.com',
+            firebase_id: nil
+          }
+        }
+
+        post '/v1/users', params: user_params
+        expect(response).to have_http_status(422)
+      end
+    end
+  end
+
+  describe 'PUT #update (not logged in)' do
+    let!(:user3) { FactoryBot.create(:user) }
+    let!(:headers3) { { 'Authorization' => JsonWebToken.encode(user_email: user3.email) } }
+    it 'returns 401' do
+      put "/v1/#{user3.username}"
+      expect(response).to have_http_status(401)
+      expect(response.body).to eq("{\"errors\":\"Authorization token is missing\"}")
+    end
+
+    it 'returns 401 error if the user does not exist' do
+      put "/v1/nonexistent_user"
+      expect(response).to have_http_status(401)
+      expect(response.body).to eq("{\"errors\":\"Authorization token is missing\"}")
+    end
+  end
+
+  describe 'PUT #update (logged in)' do
+    context 'when the user is the owner' do
+      let!(:user4) { FactoryBot.create(:user) }
+      let!(:headers4) { { 'Authorization' => JsonWebToken.encode(user_email: user4.email) } }
+      it 'updates the user' do
+        user_params = {
+          user: {
+            nickname: 'new_nickname',
+            username: 'new_username',
+            bio: 'new_bio'
+          },
+          current_user_id: user4.id
+        }
+
+        put "/v1/#{user4.username}", params: user_params, headers: headers4
+        expect(response).to have_http_status(204)
+
+        user4.reload
+        expect(user4.nickname).to eq('new_nickname')
+        expect(user4.username).to eq('new_username')
+        expect(user4.bio).to eq('new_bio')
+      end
+    end
+
+    context 'when the user is not the owner' do
+      let!(:user5) { FactoryBot.create(:user, nickname: 'user5', username: 'user5', bio: 'user5') }
+      let!(:user6) { FactoryBot.create(:user) }
+      let!(:headers6) { { 'Authorization' => JsonWebToken.encode(user_email: user6.email) } }
+      it 'returns forbidden status' do
+        user_params = {
+          user: {
+            nickname: 'new_nickname',
+            username: 'new_username',
+            bio: 'new_bio'
+          },
+          current_user_id: user6.id
+        }
+
+        put "/v1/#{user5.username}", params: user_params, headers: headers6
+        expect(response).to have_http_status(403)
+
+        user5.reload
+        expect(user5.nickname).to eq('user5')
+        expect(user5.username).to eq('user5')
+        expect(user5.bio).to eq('user5')
+      end
+    end
+
+    context 'if the user does not exist' do
+      it 'returns 404 error' do
+        user_params = {
+          user: {
+            nickname: 'new_nickname',
+            username: 'new_username',
+            bio: 'new_bio'
+          },
+          current_user_id: user1.id
+        }
+
+        put "/v1/nonexistent_user", params: user_params, headers: headers1
+        expect(response).to have_http_status(404)
+        expect(response.body).to eq("{\"errors\":\"User not found\"}")
+      end
+    end
+  end
+
+  # todo: describe 'GET #destroy (not logged in)' do
+  # todo: describe 'GET #destroy (logged in)' do
 end
