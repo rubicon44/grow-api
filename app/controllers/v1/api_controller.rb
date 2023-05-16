1d7ee20bb590a34bb3e97f54de752912aa2d4889
rubicon44 <rub.b.c44@gmail.com>
2023-05-16 13:40:36 +0900

    Fix csrf validation & Add csrf validation error method.

--- app/controllers/v1/api_controller.rb
+++ app/controllers/v1/api_controller.rb
@@ -2,6 +2,7 @@ module V1
   class ApiController < ApplicationController
     before_action :check_authenticate!
     rescue_from StandardError, with: :render_500
+    rescue_from ActionController::InvalidAuthenticityToken, with: :handle_invalid_csrf_token
     rescue_from ActiveRecord::RecordInvalid, with: :render_422
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
     rescue_from ActionController::BadRequest, with: :render_400
@@ -10,7 +11,6 @@ module V1
 
     def check_authenticate!
       token = request.cookies['token']
-      # csrf_token = request.headers['X-CSRF-Token']
 
       if token.blank?
         render json: { errors: "Authorization token is missing" }, status: :unauthorized
@@ -20,21 +20,16 @@ module V1
         decoded = JsonWebToken.decode(token)
         current_user = User.find_by(email: decoded[:user_email])
 
-        # origin_csrf_token = cookies['X-CSRF-Token']
-        # unless csrf_token == origin_csrf_token
-        #   render json: {
-        #     errors: "Invalid CSRF token",
-        #     "csrf_token": "#{csrf_token}",
-        #     "origin_csrf_token": "#{origin_csrf_token}"
-        #     }, status: 401
-        # end
-
         return current_user
       rescue ActiveRecord::RecordNotFound => e
         render json: { errors: e.message }, status: :unauthorized
       rescue JWT::DecodeError => e
         render json: { errors: e.message }, status: :unauthorized
       end
+    end
+
+    def handle_invalid_csrf_token(exception)
+      render json: { errors: 'Invalid CSRF token' }, status: 422
     end
 
     def render_500(errors)
